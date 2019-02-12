f9be29d8d8073d8d6d8023da4888536496b82c41
Loonah <lunamehtz@gmail.com>
2019-02-12 13:10:53 -0800

    random ending

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ __copyright__ = "Copyright (c) 2019 Isabel Luna"
 __credits__ = ["Luna, Isabel"]
 __license__ = "MIT lisence"
 
+import random
+
 def endings():
     ending1 = 'You died'
     ending2 = 'You become hero'
     ending3 = 'Live ordinary life'
+    ending_list = [ending1, ending2, ending3]
+    chosen_ending = random.choice(ending_list)
+    print(chosen_ending)
