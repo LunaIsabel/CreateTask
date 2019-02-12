a00bb478a45a04fd38a5e3cef3b86e6c7eec81a6
Loonah <lunamehtz@gmail.com>
2019-02-12 13:08:07 -0800

    basic endings

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ __author__ = "Luna"
 __copyright__ = "Copyright (c) 2019 Isabel Luna"
 __credits__ = ["Luna, Isabel"]
 __license__ = "MIT lisence"
+
+def endings():
+    ending1 = 'You died'
+    ending2 = 'You become hero'
+    ending3 = 'Live ordinary life'
