42e82425268879fa70482bcd571c08340f47844b
Luna <39286555+Loonah@users.noreply.github.com>
2019-02-23 22:06:10 -0800

    Merging

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ __author__ = "Luna"
 __copyright__ = "Copyright (c) 2019 Isabel Luna"
 __credits__ = ["Luna, Isabel"]
 __license__ = "MIT lisence"
+
+def endings():
+    ending1 = 'You died'
+    ending2 = 'You became the leader'
+    ending3 = 'You saved people continued to live oridinary life'
